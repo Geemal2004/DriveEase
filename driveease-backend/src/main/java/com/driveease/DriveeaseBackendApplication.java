0030f3c74c29282e3e936607ea0296737a2aac5a
geemal2004 <geemalmuthugala@gmail.com>
2026-05-25 13:45:16 +0530

    initial structure

---
+++ driveease-backend/src/main/java/com/driveease/DriveeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.driveease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveeaseBackendApplication.class, args);
+	}
+
+}
