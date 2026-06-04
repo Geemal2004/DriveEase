262580df7c4bb29c14b0f81c202a03a9d377e7cb
Geemal Muthugala <geemalmuthugala@gmail.com>
2026-06-04 10:21:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://geemal204-driveease.hf.space",
   headers: {
     "Content-Type": "application/json",
   },
